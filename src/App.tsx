68ac3d1614562591da903d09875aa0eae524acdf
emmatroni <66473121+emmatroni@users.noreply.github.com>
2025-09-29 15:28:49 +0200

    tailwind setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./styles/App.css";
+import "./index.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="min-h-screen bg-red-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">Test Tailwind</h1>
+    </div>
+  );
 }
 
 export default App;
